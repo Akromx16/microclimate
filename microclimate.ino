2dc19a4d3183f64213a744eb016d4f9d5f458dd8
Kasi Reddy <kasi.reddy@intel.com>
2016-07-12 17:29:08 +0100

    added broadcasting

--- microclimate.ino
+++ microclimate.ino
@@ -4,7 +4,16 @@
 #include <Adafruit_BMP085_U.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <CurieBLE.h>
 
+BLEPeripheral blePeripheral;
+BLEService climateService("181A"); 
+
+BLEFloatCharacteristic Altitude("2AB3", BLERead | BLENotify);
+BLEFloatCharacteristic Humidity("2A6F", BLERead | BLENotify);
+BLEFloatCharacteristic Light("2A76", BLERead | BLENotify);
+BLEFloatCharacteristic Pressure("2A6D", BLERead | BLENotify);
+BLEFloatCharacteristic Temperature("2A6E", BLERead | BLENotify);
 
 Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
 Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
@@ -23,7 +32,6 @@ void setup() {
     tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
   // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
   // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
-  
   }
 
   if(!bmp.begin())
@@ -36,14 +44,30 @@ void setup() {
   dht.temperature().getSensor(&sensor);
   dht.humidity().getSensor(&sensor);
   delayMS = sensor.min_delay / 1000;
+
+  blePeripheral.setLocalName("ClimateMonitor");
+  blePeripheral.setAdvertisedServiceUuid(climateService.uuid());  // add the service UUID
+  blePeripheral.addAttribute(Altitude);
+  blePeripheral.addAttribute(Humidity);
+  blePeripheral.addAttribute(Light);
+  blePeripheral.addAttribute(Pressure);
+  blePeripheral.addAttribute(Temperature);
+  Altitude.setValue(0);
+  Humidity.setValue(0);
+  Light.setValue(0);
+  Pressure.setValue(0);
+  Temperature.setValue(0);
+  blePeripheral.begin();
 }
 
 void loop() {
+  blePeripheral.poll();
   sensors_event_t event;
   
   tsl.getEvent(&event);
   if (event.light)
   {
+    Light.setValue(event.light);
     Serial.print("Light:       "); Serial.print(event.light); Serial.println(" lux");
   } else
   {
@@ -51,40 +75,19 @@ void loop() {
   }
 
   bmp.getEvent(&event);
+  
   if (event.pressure)
   {
+    Pressure.setValue(event.pressure);
     Serial.print("Pressure:    ");
     Serial.print(event.pressure);
     Serial.println(" hPa");
-    
-    /* Calculating altitude with reasonable accuracy requires pressure    *
-     * sea level pressure for your position at the moment the data is     *
-     * converted, as well as the ambient temperature in degress           *
-     * celcius.  If you don't have these values, a 'generic' value of     *
-     * 1013.25 hPa can be used (defined as SENSORS_PRESSURE_SEALEVELHPA   *
-     * in sensors.h), but this isn't ideal and will give variable         *
-     * results from one day to the next.                                  *
-     *                                                                    *
-     * You can usually find the current SLP value by looking at weather   *
-     * websites or from environmental information centers near any major  *
-     * airport.                                                           *
-     *                                                                    *
-     * For example, for Paris, France you can check the current mean      *
-     * pressure and sea level at: http://bit.ly/16Au8ol                   */
-     
-    /* First we get the current temperature from the BMP085 */
-    float temperature;
-    bmp.getTemperature(&temperature);
-    Serial.print("Temperature: ");
-    Serial.print(temperature);
-    Serial.println(" C");
 
-    /* Then convert the atmospheric pressure, and SLP to altitude         */
-    /* Update this next line with the current SLP for better results      */
-    float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
+    float alt = bmp.pressureToAltitude(SENSORS_PRESSURE_SEALEVELHPA,
+                                        event.pressure);
+    Altitude.setValue(alt);
     Serial.print("Altitude:    "); 
-    Serial.print(bmp.pressureToAltitude(seaLevelPressure,
-                                        event.pressure)); 
+    Serial.print(alt); 
     Serial.println(" m");
     
   }
@@ -98,6 +101,7 @@ void loop() {
     Serial.println("Error reading temperature!");
   }
   else {
+    Temperature.setValue(event.temperature);
     Serial.print("Temperature: ");
     Serial.print(event.temperature);
     Serial.println(" *C");
@@ -108,6 +112,7 @@ void loop() {
     Serial.println("Error reading humidity!");
   }
   else {
+    Humidity.setValue(event.relative_humidity);
     Serial.print("Humidity: ");
     Serial.print(event.relative_humidity);
     Serial.println("%");
